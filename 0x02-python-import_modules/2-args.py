75d4032a94dd70c77463eb754be81e72fe24a3a6
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-04-30 22:37:21 -0500

    other change in len_argv for len_entradasys

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     elif len_entradasys == 2:
         print("1: {}".format(sys.argv[1]))
     else:
-        print("{:d} arguments:".format(len_argv - 1))
+        print("{:d} arguments:".format(len_entradasys - 1))
         for i in range(1, len_entradasys):
             print("{:d}: {}".format(i, sys.argv[i]))
 
