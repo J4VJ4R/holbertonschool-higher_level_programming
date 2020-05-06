48e6ca4049ac697cdb9f6f05289eec984497767e
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-05 20:27:03 -0500

    len(sentence) == 0

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
-        val = 0
-        return sentence[val], None
+    if len(sentence) == 0:
+        return len(sentence), None
     else:
         return len(sentence), sentence[0]
