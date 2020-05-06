1dceeabda09d29a1f7c9a8c3c3028b17971eb225
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-05 20:19:10 -0500

    I GO think in c o my God

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence is None:
-        val = 0;
+        val = 0
         return sentence[val]
     else:
         return len(sentence), sentence[0]
