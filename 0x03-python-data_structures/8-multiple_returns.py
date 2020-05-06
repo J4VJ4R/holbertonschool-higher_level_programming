7c4453a8639512a05036ab14f5b52d0f10b77a10
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-05 20:15:02 -0500

    I less a litle for my owner code

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,5 @@ def multiple_returns(sentence):
     if sentence is None:
         val = 0;
         return sentence[val]
-    return len(sentence)
+    else
+        return len(sentence), sentence[0]
