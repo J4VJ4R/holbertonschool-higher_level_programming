ae003e36beb8abc259ac457e462a04a450e842e5
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-05 20:24:47 -0500

    I less second value in teh result

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     if sentence is None:
         val = 0
-        return sentence[val]
+        return sentence[val], None
     else:
         return len(sentence), sentence[0]
