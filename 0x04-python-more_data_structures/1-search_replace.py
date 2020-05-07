6f4136151652f1c9f1a615d4b4eb2c02032c75bc
Javier <ingenierodesoftware73@gmail.com>
2020-05-07 19:44:17 +0000

    a function that replace an element

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list[]
+    new_list = []
     for val in my_list:
         if val == search:
             new_list.append(replace)
