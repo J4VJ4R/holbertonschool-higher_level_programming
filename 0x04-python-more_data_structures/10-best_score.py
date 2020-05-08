e4debfa8465fad087b5315635dbab9b3661a88e9
Javier <ingenierodesoftware73@gmail.com>
2020-05-08 01:47:49 +0000

    755 *.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    score_max = max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
-    return score_max
+    s_max = max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
+    return s_max
