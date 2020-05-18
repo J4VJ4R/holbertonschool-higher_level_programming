ab425db3817d3bd6f9b72f1a1ac4063c08436443
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-18 10:42:26 -0500

    fix the error in the for

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     lap = 0
-    for i range(x):
+    for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
             lap += 1
