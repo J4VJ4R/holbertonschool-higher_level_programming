d496c067626f14fa9dd3633ad6eb738e87739b2a
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-19 17:48:13 -0500

    adding two new lines after the comments

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This class define a square."""
+
+
 class Square:
-    """Here you can write attributes and
-    methods."""
+    """Here you can write attributes
+    and methods."""
     pass
