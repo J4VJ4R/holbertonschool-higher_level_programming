a5d039c9c094cf07790c7ef6e51fd8c12564a291
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-19 17:44:22 -0500

    class to create a square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""This class define a square"""
+class Square:
+    """Here you can write attributes and
+    methods"""
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,2 @@
+#CLASSES#
+#POO in Python and the first step is create a different classes#
