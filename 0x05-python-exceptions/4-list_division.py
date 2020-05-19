5e71278928b39acc6bbae228d63dd8e98a30cdd5
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-19 12:27:34 -0500

    I missing a letter ouchh

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def list_division(my_list_1, my_list_2, list_leng):
+def list_division(my_list_1, my_list_2, list_lengh):
     new_list = []
     for i in range(0, list_length):
         try:
