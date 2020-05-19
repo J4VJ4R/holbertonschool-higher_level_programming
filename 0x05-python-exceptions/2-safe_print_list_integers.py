ab2cd66c8fa414c2f410d8cefaf2c19d84e0daac
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-19 13:54:05 -0500

    I missing a parentesis

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     valReturn = 0
     for i in range(0, x):
         try:
-            print("{:d}".format(my_list[i], end=""))
+            print("{:d}".format(my_list[i]), end="")
         except (ValueError, TypeError):
             continue
     print("")
