a9edd1e1812f5319ee32055ea84cba5c2dbce975
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-19 19:44:50 -0500

    I needed change a value

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -12,17 +12,19 @@ class Square:
             size (int): Size of the square.
         """
         self.size = size
+
     @property
     def size(self):
         """Get/set the actual size for the square"""
         return (self.__size)
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
         """Return the current square area"""
