92007dc58964365108c6eb43f82d1a6377c644f5
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-23 11:39:58 -0500

    change in all code

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
+
 """
-This is a function it does
-a different tests with the use
-edge cases.
+    this module is to add two integers
+    and only must be integers.
 """
 
 
 def add_integer(a, b=98):
-    """ this is a function that
-    return an add with two integers
     """
-
-    if type(a) != int and type(a) != float:
-        raise TypeError("a must be an integer")
+        must be a return of a integer
+    """
+    if type(a) != int and type(b) != float:
+        raise TypeError('a must be an integer')
     if type(b) != int and type(b) != float:
-        raise TypeError("b must be an integer")
-
-    if type(a) == float:
-        a = int(a)
-    if type(b) == float:
-        b = int(b)
-
+        raise TypeError('b must be an integer')
     return int(a) + int(b)
