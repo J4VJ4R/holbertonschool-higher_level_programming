e52cd87b18fdb6ef0341797630a1451893755cbb
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-25 16:33:24 -0500

    invalid sintax

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -46,6 +46,6 @@ class Rectangle:
 
     def perimeter(self):
         """returns the rectangle perimeter"""
-        if (self.__height) = 0 or (self.__width) = 0:
+        if (self.__height) == 0 or (self.__width) == 0:
             return 0
         return ((2 * self.__height) + (2 * self.width) )
