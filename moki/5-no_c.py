a80d79ba69fef5eff037c4102d1323827b986ebe
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-06-01 14:59:21 -0500

    changes

--- moki/5-no_c.py
+++ moki/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
     for i in my_string:
-        if (my_string[i] == 'c' & my_string[i] == 'C'):
+        if ((my_string[i] == 'c') & (my_string[i] == 'C')):
             return i
