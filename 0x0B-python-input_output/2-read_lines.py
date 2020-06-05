6eecbf35dd3e062b198d79e59c5c4d6343d04bb0
Javier <ingenierodesoftware73@gmail.com>
2020-06-05 17:49:53 +0000

    a function that reads n lines of a text file (UTF8) and prints it to stdout:

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -10,7 +10,7 @@ a function that reads n lines of a text file
 def read_lines(filename="", nb_lines=0):
     """ Return a n number of lines of the text """
     i = len(open(filename).readlines())
-    with open(filename, "r") as file:
+    with open(filename, 'r') as file:
         if nb_lines > 0 and nb_lines < i:
             while nb_lines:
                 print(file.read_line(), end="")
