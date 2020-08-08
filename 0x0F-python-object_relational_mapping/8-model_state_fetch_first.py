5fa99407f32778b8000e0aac4b5af2f2b10aa4e2
Javier <ingenierodesoftware73@gmail.com>
2020-08-08 14:25:19 +0000

    task 8 first state with SQLAlchemy

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
-"""first state"""
+"""first state with SQLAlchemy"""
 
+import sys
+import json
 from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sesionmaker
-import sys
+from sqlalchemy.orm import sessionmaker
 
-fi __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    DB_name = sys.argv[3]
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
-    engine = create_engine('mysql+mysql://{}:{}@localhost')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).first()
+
+    if isinstance(state, State):
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
