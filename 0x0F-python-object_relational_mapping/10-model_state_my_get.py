a365c012e2faa2d816b8059562992ca6435a2058
Javier <ingenierodesoftware73@gmail.com>
2020-08-08 22:58:11 +0000

    uploadin task 10 get state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""a script that prints the State object with the name
+passed as argument from the database hbtn_0e_6_usa"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv.argv[1]
+    password = sys.argv[2]
+    DB_name = sys.argv[3]
+    state_name = sys.argv[4]
+    
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password,, DB_name),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    state = session.query(State)
+    res = states.filter_by(name=state_name).first()
+    print(res.id if res else "Not found")
+    session.close()
