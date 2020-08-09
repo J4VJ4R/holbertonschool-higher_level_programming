c27db2f09123d6536f4e421ce8ab336b6aa359b0
Javier <ingenierodesoftware73@gmail.com>
2020-08-09 14:19:42 +0000

    add Lousiana

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""a script that prints the State object with the name
+passed as argument from the database hbtn_0e_6_usa"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    DB_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, DB_name),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    lousiana = State(name="Louisiana")
+    session.add(lousiana)
+    
+    print(session.query(State).filter_by(name="Lousiana").first().id)
+    session.commit()
+    session.close()
