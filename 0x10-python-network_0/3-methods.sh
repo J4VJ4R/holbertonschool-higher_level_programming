d923b505cd958d55d9a77d8aff3279e5307fe437
Javier <ingenierodesoftware73@gmail.com>
2020-08-19 02:56:53 +0000

    only bash

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #a Bash script that takes in a URL and displays all HTTP methods the server will accept
 curl -s -X OPTIONS -i "$1" | grep -o -P "(?<=Allow: ).*(?=[\r|\n|$])"
