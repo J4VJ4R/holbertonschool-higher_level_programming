afa1aa2306d6b18337459f25c91ffd941a292633
Javier <ingenierodesoftware73@gmail.com>
2020-08-25 04:35:03 +0000

    Write a Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a letter and
+sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) < 2:
+        q = ""
+    else:
+        q = sys.argv[1]
+    payload = {"q": q}
+
+    r = requests.post(url, data=payload)
+    try:
+        valid = r.json()
+    except ValueError:
+        print("Not a valid JSON")
+    else:
+        if r.text == "{}\n" or r.text == "{}" or \
+           r.text == "" or r.text is None:
+            print("No result")
+        else:
+            r_dict = dict([e.split(":") for e in
+                           r.text.strip("{}\n").replace('"', '').split(",")])
+            print("[{id}] {name}".format(**r_dict))
