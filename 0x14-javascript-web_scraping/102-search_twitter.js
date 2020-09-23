3499c76a9af1a247b9815d6551c822fccb7db0f6
Javier <ingenierodesoftware73@gmail.com>
2020-09-23 01:31:45 +0000

    upload tasks

---
+++ 0x14-javascript-web_scraping/102-search_twitter.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const request = require('request');
+const CONSUMER_KEY = process.argv[2];
+const CONSUMER_SECRET = process.argv[3];
+const SEARCH_STRING = process.argv[4];
+const oauth =
+    {
+      consumer_key: CONSUMER_KEY,
+      consumer_secret: CONSUMER_SECRET
+    };
+const query = {
+  q: SEARCH_STRING,
+  count: 5
+};
+const AuthData = { grant_type: 'client_credentials' };
+let url = 'https://api.twitter.com/oauth/request_token';
+request.post({ url: url, auth: oauth, data: AuthData }, function (e, r, body) {
+  url = 'https://api.twitter.com/1.1/search/tweets.json';
+  request.get({ url: url, oauth: oauth, qs: query, json: true }, function (e, r, users) {
+    for (const user of users.statuses) {
+      const TweetID = user.id;
+      const TweetText = user.text;
+      const TweetOwner = user.user.name;
+      console.log(`[${TweetID}] ${TweetText} by ${TweetOwner}`);
+    }
+  });
+});
---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    let count = 0;
+
+    for (const film of data.results) {
+      for (const char of film.characters) {
+        if (char.endsWith('18/')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
